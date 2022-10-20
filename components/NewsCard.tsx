e652cb8827c4f9059356a8f456bcb4d7901c9324
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2022-10-20 22:57:48 +0100

    Minor tweak to the card component

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -5,31 +5,33 @@ export const NewsCard = (props: TArticle) => {
   return (
     <Link href={props.url}>
       <a className="hover:opacity-60">
-        <article className="flex justify-between items-start border-2 border-t border-line mx-6 py-5">
-          <header className="basis-2/3 md:basis-1/3 pr-2">
-            <h2 className="mb-6 text-xlm font-semibold">
-              <a href={props.url}>{props.title}</a>
-            </h2>
-            <div>
-              <div className="text-smx">
-                <span className="font-bold">{props.author}</span>
-                <div>
-                  <span className="font-extralight">{props.publishedAt}</span>
+        <div>
+          <article className="flex justify-between items-start border-2 border-t border-line mx-6 py-5">
+            <header className="basis-2/3 md:basis-1/3 pr-2">
+              <h2 className="mb-6 text-xlm font-semibold">
+                <span>{props.title}</span>
+              </h2>
+              <div>
+                <div className="text-smx">
+                  <span className="font-bold">{props.author}</span>
+                  <div>
+                    <span className="font-extralight">{props.publishedAt}</span>
+                  </div>
                 </div>
               </div>
+            </header>
+            <div className="basis-1/3 px-4 hidden md:block">
+              <p>{props.description}</p>
             </div>
-          </header>
-          <div className="basis-1/3 px-4 hidden md:block">
-            <p>{props.description}</p>
-          </div>
-          <footer className="block basis-1/3 md:basis-1/3 overflow-hidden rounded-lg object-center">
-            <img
-              className="w-full"
-              src={props.urlToImage}
-              alt="Newscard image"
-            />
-          </footer>
-        </article>
+            <footer className="block basis-1/3 md:basis-1/3 overflow-hidden rounded-lg object-center">
+              <img
+                className="w-full"
+                src={props.urlToImage}
+                alt="Newscard image"
+              />
+            </footer>
+          </article>
+        </div>
       </a>
     </Link>
   );
